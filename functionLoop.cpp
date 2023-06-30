7ed8cefac6a41fc91647df813057eaccad08fbe0
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-06-29 20:26:47 -0700

    FunctionLoop

---
+++ functionLoop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void myFunction(int counter);
+
+int main(){
+   
+        cout << "Dinar Ibragimov - 07/23/22" << endl;
+    int counter;
+    
+       return 0 ;
+    }
+    void myFunction(int counter){
+    while (counter != 0){
+        cout << counter << endl;
+        counter--;
+        return;
+        }
+    }
