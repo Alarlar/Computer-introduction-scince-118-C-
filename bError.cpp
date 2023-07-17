62d7641c069ae8c80fbcf1d448c5748046900e0e
Alarlar <52812499+Alarlar@users.noreply.github.com>
2023-07-17 10:04:38 -0700

    bError

---
+++ bError.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout <<"Dinar Ibragimov - 07/17/22" <<endl;
+    
+    
+    double array[10000000] = {0}; // Declare and initialize
+    return 0;
+}
